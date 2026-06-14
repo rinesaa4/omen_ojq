26e9acf11b5c127617e80cfdf1cdb909152cd4a5
rinesa <rinesa.dushi4@gmail.com>
2026-06-14 14:40:06 +0200

    Fix router path

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-<HashRouter>
-  <App />
-</HashRouter>
+  <BrowserRouter basename="/omen_ojq">
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
